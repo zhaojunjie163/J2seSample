f7bc9a63cf3625a59b086cef443969686a95b71e
zhaojunjie163 <coastline2050@gmail.com>
2016-11-03 11:20:38 +0800

    awesome project

--- src/com/java/sample/Sample.java
+++ src/com/java/sample/Sample.java
@@ -5,9 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello git");
-<<<<<<< HEAD
 		System.out.println("test git");
-=======
->>>>>>> origin/master
+
 	}
 }
